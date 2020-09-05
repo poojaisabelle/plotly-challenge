9d1705142185597b5d19aa2493117d26dc37c407
Pooja <pooja.isabelle@gmail.com>
2020-09-05 15:05:44 +1000

    Added all starter files

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+// Use d3.json() to feth the data from the JSON file 
+d3.json("data/samples.json").then((incomingData) => {
+    console.log(incomingData);
+})
+
+
+
+
+
+// barplot 
+/// html link -> 
+
+
+
+// bubble chart 
+/// html link -> 
+
+
+
+// demographic info meta data - json 
+ 
